7ca9230bac6087c6299b0f6c2ee1d14b3c247c75
JoseAndresGGiron <titogg@byui.edu>
2023-04-02 22:22:49 -0400

    SMALL CHANGES TO MY FINAL PROJECT

--- script/searchbar.js
+++ script/searchbar.js
@@ -1,51 +1,36 @@
-// const searchForm = document.querySelector('form');
-// const searchBar = document.querySelector('#search-bar').value;
-// const recipeContainer = document.querySelector('#recipe-container');
-
-// const baseURL = "https://api.spoonacular.com/recipes/";
-// const key = "9364ffa2e0f84be6902d065f72360c25";
-
-// searchForm.addEventListener('submit', (e) => {
-//   e.preventDefault();
-
-//   const apiUrl = `${baseURL}complexSearch?apiKey=${key}&query=${searchBar}&number=20`;
-
-//   fetch(apiUrl)
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log(data);
-//       const recipes = data.results;
-//       recipeContainer.innerHTML = '';
-//       recipes.forEach(recipe => {
-//         const recipeCard = createRecipeCard(recipe);
-//         recipeContainer.appendChild(recipeCard);
-//       });
-//     })
-//     .catch(error => console.error(error));
-// });
-
-// function createRecipeCard(recipe) {
-//   const recipeCard = document.createElement('div');
-//   recipeCard.classList.add('recipe-card');
-//   recipeCard.innerHTML = `
-//     <li>  <a><img src="${recipe.image}" alt="${recipe.title}"></a>
-//     <h3>${recipe.title}</h3>
-//   </li>`;
-
-//   // add event listener to recipe card
-//   recipeCard.addEventListener('click', () => {
-//     const recipeId = recipe.id;
-//     window.location.href = `recipe.html?id=${recipeId}`;
-//   });
-
-//   return recipeCard;
-// }
 
 const searchForm = document.querySelector('form');
 const recipeContainer = document.querySelector('#recipe-container');
 const randomSearch = document.querySelector('#randomSearch-btn');
 const baseURL = "https://api.spoonacular.com/recipes/";
 const key = "9364ffa2e0f84be6902d065f72360c25";
+
+const cuisineContainer = document.getElementById('cuisine-container');
+
+// Fetch cuisines from JSON file
+fetch('https://raw.githubusercontent.com/JoseAndresGGiron/wdd330/main/src/json/cuisines.json')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    // Create <a> element for each cuisine
+    data.cuisines.forEach(cuisine => {
+      const cuisineLink = document.createElement('a');
+      cuisineLink.href = "#";
+      cuisineLink.textContent = cuisine.name;
+
+      cuisineLink.addEventListener('click', () => {
+
+        const apiUrl = `${baseURL}complexSearch?apiKey=${key}&number=10&cuisine=${cuisine.name}`;
+        fetchRecipes(apiUrl)
+          .catch(error => console.error(error));
+      });
+      const cuisineListItem = document.createElement('li');
+      cuisineListItem.appendChild(cuisineLink);
+
+      cuisineContainer.appendChild(cuisineListItem);
+    });
+  })
+  .catch(error => console.error(error));
 
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -59,7 +44,7 @@ searchForm.addEventListener('submit', (e) => {
 
 function randomRecipes() {
   const randomOffset = Math.floor(Math.random() * 20);
-  let apiUrl = `${baseURL}complexSearch?apiKey=${key}&number=20&offset=${randomOffset}`;
+  let apiUrl = `${baseURL}complexSearch?apiKey=${key}&number=10&offset=${randomOffset}`;
   fetchRecipes(apiUrl);
 }
 
@@ -79,12 +64,12 @@ function fetchRecipes(apiUrl) {
 }
 
 function createRecipeCard(recipe) {
-  const recipeCard = document.createElement('div');
+  const recipeCard = document.createElement('li');
   recipeCard.classList.add('recipe-card');
   recipeCard.innerHTML = `
-    <li>  <a><img src="${recipe.image}" alt="${recipe.title}"></a>
-    <h3>${recipe.title}</h3>
-  </li>`;
+    <a><img src="${recipe.image}" alt="${recipe.title}"></a>
+    <h3>${recipe.title}</h3>`;
+  
 
   // add event listener to recipe card
   recipeCard.addEventListener('click', () => {
